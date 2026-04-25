b809113b035ca071eff985cc458efc0d27dbe2a8
Rusmir Omerovic <rusmir.omerovic@web.de>
2026-04-25 14:02:39 +0200

    read database_url from environment variable

--- app/database.py
+++ app/database.py
@@ -1,9 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://admin:admin@db:5432/jobs"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://admin:admin@db:5432/jobs"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
