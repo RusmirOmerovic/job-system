38d84d002cd35de77c07c715def1e201cf1686e5
Rusmir Omerovic <rusmir.omerovic@web.de>
2026-04-25 14:38:48 +0200

    add health + root endpoint

--- app/main.py
+++ app/main.py
@@ -5,4 +5,16 @@ from .routes import router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# 👉 NEU
+@app.get("/")
+def root():
+    return {"message": "Job System API running"}
+
+# 👉 NEU
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+# bestehend
 app.include_router(router)
